9eff53856c9a63bbc7f17686b723255ebc9f4459
MSI\boltk <boltkun.piku@gmail.com>
2025-10-24 15:33:35 +0900

    button24

--- Form_Test/Form1.cs
+++ Form_Test/Form1.cs
@@ -15,6 +15,33 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                //　インスタンスの生成
+                {
+                    test_button test_Button = new test_button();
+
+
+                    //ボタンの位置を設定
+                    test_Button.Location = new Point(50 * j, 50 * i);
+
+                    //ボタンの大きさを設定
+                    test_Button.Size = new Size(50, 50);
+
+                    //ボタン内のテキストを設定
+                    test_Button.Text = "あいうえお";
+                    //コントロールにボタンを追加
+                    Controls.Add(test_Button);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ハト");
         }
     }
 }
---
+++ Form_Test/test-button.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Test
+{
+    internal class test_button : Button
+    {
+
+    }
+
+}
+
