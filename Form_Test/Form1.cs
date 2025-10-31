75ac1597b51cf735ed58e222c68e408133681e43
MSI\boltk <boltkun.piku@gmail.com>
2025-10-31 15:33:52 +0900

    色変更

--- Form_Test/Form1.cs
+++ Form_Test/Form1.cs
@@ -11,18 +11,25 @@ using System.Windows.Forms;
 namespace Form_Test
 {
     public partial class Form1 : Form
-    {
+    {//constをつけると初期値のみ変更できる
+        const int BUTTUN_SIZE_X = 100;
+        const int BUTTUN_SIZE_Y = 100;
+        const int BOARD_SIZE_X = 3;
+        const int BOARD_SIZE_Y = 3;
+
+
         public Form1()
         {
             InitializeComponent();
 
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < BOARD_SIZE_X; i++)
             {
-                for (int j = 0; j < 6; j++)
+                for (int j = 0; j < BOARD_SIZE_Y; j++)
                 //　インスタンスの生成
                 {
-                    test_button test_Button = new test_button(new Point(50 * j, 50 * i), new Size(50, 50), "あ");
+                    test_button test_Button = new test_button(new Point(BUTTUN_SIZE_X * j, BUTTUN_SIZE_Y * i),
+                                              new Size(BUTTUN_SIZE_X, BUTTUN_SIZE_Y), "あ");
 
                     
                     //コントロールにボタンを 追加
--- Form_Test/test-button.cs
+++ Form_Test/test-button.cs
@@ -10,18 +10,45 @@ namespace Form_Test
 {
     internal class test_button : Button
     {
+    private Color _onColor = Color.Purple;
+    private Color _offColor = Color.White;
+        private bool _enable;
+
+        public void SetEnable(bool on)
+        {
+            _enable = on;
+            if (on)
+            {
+                BackColor = _onColor;
+            }
+            else
+            {
+                BackColor = _offColor;
+            }
+        }
+
+
+
+
         public test_button(Point position, Size size, string text)
         {
             Location = position;
             Size = size;
             Text = text;
+
+            SetEnable(true);
+
+            Click += Click1Event;
         }
-     
-        private void  ClickEvent (object sender, EventArgs e) 
+        
+        private void  Click1Event (object sender, EventArgs e) 
         {
+
+
             MessageBox.Show("ハト");
+            SetEnable(!_enable);
         }
     }
-
+    
 }
 
