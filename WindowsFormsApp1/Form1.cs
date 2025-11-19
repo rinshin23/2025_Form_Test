9ecc0c2816f84f653406fd980167c8e03c941362
MSI\boltk <boltkun.piku@gmail.com>
2025-11-19 15:43:18 +0900

    ?

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -8,15 +9,21 @@ namespace BomberMini
     {
         Button[,] map = new Button[9, 9];
         Random rand = new Random();
+        bool bombPlaced = false;
+
+        PictureBox player;
+        Point playerPos;
 
         public Form1()
         {
             InitializeComponent();
             this.Load += Form1_Load;
+            this.Shown += (s, e) => this.Focus(); // フォーカスを強制
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // マップ生成
             for (int y = 0; y < 9; y++)
             {
                 for (int x = 0; x < 9; x++)
@@ -25,29 +32,65 @@ namespace BomberMini
                     btn.Size = new Size(40, 40);
                     btn.Location = new Point(x * 42, y * 42);
                     btn.Tag = new Point(x, y);
-                    btn.Click += Btn_Click;
-
-                    // ランダムで壊せる壁 or 空きスペース
                     btn.BackColor = rand.Next(3) == 0 ? Color.Brown : Color.White;
-
                     map[x, y] = btn;
                     Controls.Add(btn);
                 }
             }
+
+            // プレイヤー初期位置
+            playerPos = new Point(0, 0);
+            player = new PictureBox();
+            player.Size = new Size(40, 40);
+            player.BackColor = Color.Blue;
+            player.Location = map[playerPos.X, playerPos.Y].Location;
+            Controls.Add(player);
+
+            // キー入力設定
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+           
         }
 
-        private void Btn_Click(object sender, EventArgs e)
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            var btn = sender as Button;
-            var pos = (Point)btn.Tag;
+            int x = playerPos.X;
+            int y = playerPos.Y;
 
-            btn.BackColor = Color.Red; // 爆弾設置
+            if (e.KeyCode == Keys.Up && y > 0) y--;
+            if (e.KeyCode == Keys.Down && y < 8) y++;
+            if (e.KeyCode == Keys.Left && x > 0) x--;
+            if (e.KeyCode == Keys.Right && x < 8) x++;
+
+            if (map[x, y].BackColor != Color.Brown)
+            {
+                playerPos = new Point(x, y);
+                player.Location = map[x, y].Location;
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                PlaceBomb(playerPos.X, playerPos.Y);
+            }
+        }
+
+        private void PlaceBomb(int x, int y)
+        {
+            if (bombPlaced) return;
+            if (x < 0 || y < 0 || x >= 9 || y >= 9) return;
+
+            var btn = map[x, y];
+            if (btn == null || btn.BackColor == Color.Red) return;
+
+            btn.BackColor = Color.Red;
+            bombPlaced = true;
 
             var timer = new Timer();
             timer.Interval = 3000;
             timer.Tick += (s, ev) =>
             {
-                Explode(pos.X, pos.Y);
+                Explode(x, y);
+                bombPlaced = false;
                 timer.Stop();
                 timer.Dispose();
             };
@@ -56,27 +99,51 @@ namespace BomberMini
 
         void Explode(int x, int y)
         {
+            List<Button> affected = new List<Button>();
+
             int[][] dirs = new int[][]
             {
-        new int[] { 0, 1 },
-        new int[] { 1, 0 },
-        new int[] { 0, -1 },
-        new int[] { -1, 0 }
+                new int[] { 0, 1 },
+                new int[] { 1, 0 },
+                new int[] { 0, -1 },
+                new int[] { -1, 0 }
             };
 
-            foreach (var dir in dirs)
+            for (int i = 0; i < dirs.Length; i++)
             {
-                int nx = x + dir[0], ny = y + dir[1];
-                if (nx < 0 || ny < 0 || nx >= 9 || ny >= 9) continue;
+                for (int step = 1; step <= 2; step++)
+                {
+                    int nx = x + dirs[i][0] * step;
+                    int ny = y + dirs[i][1] * step;
+                    if (nx < 0 || ny < 0 || nx >= 9 || ny >= 9) break;
 
-                var target = map[nx, ny];
-                if (target.BackColor == Color.Brown)
-                    target.BackColor = Color.White; // 壁破壊
-                else
-                    target.BackColor = Color.Yellow; // 爆風演出
+                    var target = map[nx, ny];
+                    if (target.BackColor == Color.Brown)
+                    {
+                        target.BackColor = Color.White;
+                        break;
+                    }
+                    else
+                    {
+                        target.BackColor = Color.Yellow;
+                        affected.Add(target);
+                    }
+                }
             }
 
-            map[x, y].BackColor = Color.White; // 爆弾消去
+            map[x, y].BackColor = Color.White;
+
+            var clearTimer = new Timer();
+            clearTimer.Interval = 300;
+            clearTimer.Tick += (s, e) =>
+            {
+                foreach (var b in affected)
+                    b.BackColor = Color.White;
+
+                clearTimer.Stop();
+                clearTimer.Dispose();
+            };
+            clearTimer.Start();
         }
     }
-        }
+}
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal class Form2
+    {
+      
+    }
+}
